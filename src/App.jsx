315c17e2858c830ef1e0aa15dfff33fa08c2a104
Kwonyeojiny <kangagi0009@naver.com>
2024-10-18 16:13:30 +0900

    시계 기능구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react'
 import './App.css'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
+import Clock from './components/Clock'
 
 function App() {
   const [todo, setTodo] = useState([
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <>
+      <Clock />
       <TodoInput setTodo={setTodo}/>
       <TodoList todo={todo} setTodo={setTodo}/>
     </>
